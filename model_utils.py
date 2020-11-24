953b365316f8b33f2c40e054da0b0abf1f95afa4
Junzheng <alchemistWu0521@gmail.com>
2020-11-24 18:06:10 -0500

    initial changes

---
+++ model_utils.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+def model_factory(name, pretrain=None, target_size=256):
+    """
+    getting the keras application model
+    :param name: specifying the model
+    :param pretrain: if True, load imagenet pretrained weights
+    :param target_size:
+    :return:
+    """
+    weights = None
+    if pretrain:
+        weights = 'imagenet'
+    model_dict = {
+        'res': tf.keras.applications.ResNet50,
+        'vgg': tf.keras.applications.vgg16,
+        'googLeNet': tf.keras.applications.inception_v3,
+        'dense': tf.keras.applications.DenseNet121,
+        'mobile': tf.keras.applications.MobileNetV2
+    }
+    return model_dict[name](weights=weights, include_top=False, input_shape=(target_size, target_size, 3))
+
+def get_model(name='res', pretrain=True, verbose=False, target_size=256, n_class=9):
+    """
+    Due to the API limitation, shape of MNIST has to be resized as (32, 32, 3)
+    :param pretrain: if Ture, return attack model with ImageNet weights
+    :param task: 'mnist' or 'cifar'
+    :param verbose: Print model information if True
+    :return: keras model
+    """
+    input = tf.keras.layers.Input(shape=(target_size, target_size, 3))
+    m = model_factory(name=name, pretrain=pretrain, target_size=target_size)(input)
+    flatten = tf.keras.layers.GlobalMaxPool2D()(m)
+    output = tf.keras.layers.Dense(n_class, activation='softmax')(flatten)
+    model = tf.keras.Model(inputs=[input], outputs=[output])
+    model.compile(loss=['sparse_categorical_crossentropy'], metrics=['acc'])
+    if verbose:
+        model.summary()
+    return model
+
+if __name__ == '__main__':
+    get_model(verbose=True)
---
+++ train.py
@@ -0,0 +1,164 @@
+from model_utils import *
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.callbacks import CSVLogger, EarlyStopping
+import os
+import shutil
+import argparse
+
+class SaveBestModel(tf.keras.callbacks.Callback):
+    """
+    Callbacks for saving the model with lowest val_acc
+    """
+    def __init__(self, filepath, model_name, monitor='val_loss'):
+        super(SaveBestModel, self).__init__()
+        self.model_name = model_name
+        self.best_weights = None
+        self.file_path = filepath
+        self.best = float('inf')
+        self.monitor = monitor
+
+    def on_epoch_end(self, epoch, logs=None):
+        current = logs.get(self.monitor)
+        if not current:
+            current = float('inf')
+
+        if np.less(current, self.best):
+            self.best = current
+            self.best_weights = self.model.get_weights()
+
+            if not os.path.exists(self.file_path):
+                os.mkdir(self.file_path)
+
+            new_path = os.path.join(self.file_path, self.model_name)
+            if not os.path.exists(new_path):
+                os.mkdir(new_path)
+            else:
+                shutil.rmtree(new_path)
+
+            new_path_model = os.path.join(new_path, 'model.tf')
+            self.model.save_weights(new_path_model)
+            print("New best model has been saved in %s!" % new_path_model)
+            print("Best acc: %.4f" % current)
+
+def get_tfDataset(training_directory, test_directory, verbose=False, batch_size=32):
+    """
+    loading directories into tensorflow dataset format
+    :param training_directory:
+    :param test_directory:
+    :param verbose: if Ture, showing some sample from both training and testing dataset.
+    :param batch_size:
+    :return: tensorflow dataset object
+    """
+    dataTrain = tf.keras.preprocessing.image_dataset_from_directory(
+    training_directory,
+    labels="inferred",
+    label_mode="int",
+    color_mode="rgb",
+    batch_size=batch_size,
+    image_size=(256, 256),
+    shuffle=True,
+    seed=1,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    )
+    dataTrain = dataTrain.map(lambda x, y: (x / 255., y))
+    dataTest = tf.keras.preprocessing.image_dataset_from_directory(
+        test_directory,
+        labels="inferred",
+        label_mode="int",
+        color_mode="rgb",
+        batch_size=batch_size,
+        image_size=(256, 256),
+        shuffle=False,
+        seed=1,
+        validation_split=None,
+        subset=None,
+        interpolation="bilinear",
+        follow_links=False,
+    )
+
+    dataTest = dataTest.map(lambda x, y: (x / 255., y))
+    if verbose:
+        plt.figure(figsize=(10, 10))
+        for i, (images, labels) in enumerate(dataTrain.take(1)):
+            for j in range(9):
+                ax = plt.subplot(3, 3, j + 1)
+                image = images[j, :, :, :]
+                label = labels[j]
+                plt.imshow(image, cmap='gray', vmin=0., vmax=1.)
+                plt.title(int(label))
+                plt.axis("off")
+        plt.show()
+
+        plt.figure(figsize=(10, 10))
+        for i, (images, labels) in enumerate(dataTest.take(1)):
+            for j in range(9):
+                ax = plt.subplot(3, 3, j + 1)
+                image = images[j, :, :, :]
+                label = labels[j]
+                plt.imshow(image, cmap='gray', vmin=0., vmax=1.)
+                plt.title(int(label))
+                plt.axis("off")
+        plt.show()
+    return dataTrain, dataTest
+
+def train(train_directory, test_directory, model_name='res', epoch=100, batch_size=64, multi_gpu=True, pretrain=False):
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    dataTrain, dataTest = get_tfDataset(train_directory, test_directory, batch_size=batch_size)
+    dataTrain = dataTrain.cache()
+    dataTrain = dataTrain.prefetch(tf.data.experimental.AUTOTUNE)
+    dataTest = dataTest.cache()
+    dataTest = dataTest.prefetch(tf.data.experimental.AUTOTUNE)
+
+    if multi_gpu:
+        strategy = tf.distribute.MirroredStrategy()
+    else:
+        strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
+    with strategy.scope():
+        model = get_model(name=model_name, pretrain=True, target_size=256, n_class=9)
+        if pretrain:
+            model_name += "_pretrain"
+        model.fit(dataTrain, epochs=epoch, validation_data=dataTest, verbose=1, batch_size=batch_size,
+                  callbacks=[SaveBestModel('model', model_name), CSVLogger('logs/%s.csv' % (model_name)),
+                             EarlyStopping(monitor='val_loss', patience=10)])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-single_gpu', type=bool,
+                        help='Training with single GPU support, default is multiple gpu',
+                        dest='multi_gpu', const=False, default=True, nargs='?')
+
+    parser.add_argument('-train_dir', type=str,
+                        help='Training directory',
+                        dest='train_dir', default="/home/junzheng/course/CSI5137/csi5137Project/malware-classification/train_imgs")
+
+    parser.add_argument('-test_dir', type=str,
+                        help='Test directory',
+                        dest='test_dir',
+                        default="/home/junzheng/course/CSI5137/csi5137Project/malware-classification/test_imgs")
+
+    parser.add_argument('-batch_size', type=int,
+                        help='batch size',
+                        dest='batch_size',
+                        default=32)
+
+    parser.add_argument('-epoch', type=int,
+                        help='number of epochs',
+                        dest='epoch',
+                        default=100)
+
+    parser.add_argument('-model', type=str,
+                        help=''' Models to be used, options are: 'res','vgg','googLeNet','dense','mobile', defaut is 'res' ''',
+                        dest='model',
+                        default='res')
+    args = parser.parse_args()
+
+    train(args.train_dir, args.test_dir, multi_gpu=args.multi_gpu,
+          batch_size=args.batch_size, epoch=args.epoch, model_name=args.model)
