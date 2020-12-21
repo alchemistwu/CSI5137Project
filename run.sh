d542132905de2b7a40b163e99d8a9e72940ebb04
Junzheng <alchemistWu0521@gmail.com>
2020-12-21 18:24:28 -0500

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 conda activate tfnight
+python train.py -model res
+python train.py -model res -pretrain
+python train.py -model vgg
+python train.py -model vgg -pretrain
 python train.py -model googLeNet -pretrain
 python train.py -model googLeNet
 python train.py -model mobile
 python train.py -model mobile -pretrain
 python train.py -model dense
 python train.py -model dense -pretrain
-
